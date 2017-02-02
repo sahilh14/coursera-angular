9ac6bc547f1e83c3a99f38813330b11eff270efe
Sahil gupta <sahilh14@gmail.com>
2017-02-03 03:18:34 +0530

    filter code

--- test/app.js
+++ test/app.js
@@ -40,29 +40,96 @@ angular.module('ShoppingListApp', [])
 
 ShoppingListController.$inject = ['$scope'];
 function ShoppingListController($scope) {
+  $scope.keywordarray = shoppingList;
   $scope.shoppingList = shoppingList;
   $scope.themes = themes;
   $scope.type = type;
   $scope.mythemes = {};
   $scope.mytypes = {};
+  $scope.mykeywords = {};
+  $scope.mykeywordsback = {};
   $scope.dummy = false;
   $scope.branded = false;
+  $scope.newbucket = [];
+  $scope.movekeywordsback = function () {
+    var keyword_delete = [];
+    console.log("mykeywordsback before->", $scope.mykeywordsback);
+    for (var key in $scope.mykeywordsback){
+      if ($scope.mykeywordsback.hasOwnProperty(key)){
+        if ($scope.mykeywordsback[key]){
+          keyword_delete.push(key);
+          delete $scope.mykeywordsback[key];
+        }
+      }
+    }
+    console.log("mykeywordsback after->", $scope.mykeywordsback);
+    // console.log("movekeywordsback - keyword_delete", keyword_delete);
+    for (var i = 0; i<$scope.newbucket.length; i++){
+      var obj = $scope.newbucket[i];
+      if(keyword_delete.indexOf(obj.name) !== -1){
+        $scope.newbucket.splice(i,1);
+        $scope.shoppingList.push(obj);
+        if($scope.keywordarray.indexOf(obj) == -1){
+          $scope.keywordarray.push(obj);
+        }
+        i--;
+      }
+    }
+    console.log("keywordarray -> ", $scope.keywordarray);
+    console.log("shoppingList -> ", $scope.shoppingList);
+  }
+  $scope.movekeywords = function () {
+    var keyword_delete = [];
+    console.log("mykeywords before->", $scope.mykeywords);
+    for (var key in $scope.mykeywords){
+      if ($scope.mykeywords.hasOwnProperty(key)){
+        if ($scope.mykeywords[key]){
+          keyword_delete.push(key);
+          delete $scope.mykeywords[key];
+        }
+      }
+    }
+    // console.log(keyword_delete);
+    console.log("mykeywords after->", $scope.mykeywords);
+
+    for (var i = 0; i<$scope.shoppingList.length; i++){
+      var obj = $scope.shoppingList[i];
+      if(keyword_delete.indexOf(obj.name) !== -1){
+        $scope.shoppingList.splice(i,1);
+        $scope.newbucket.push(obj);
+        i--;
+      }
+    }
+    for (var i = 0; i<$scope.keywordarray.length; i++){
+      var obj = $scope.keywordarray[i];
+      if(keyword_delete.indexOf(obj.name) !== -1){
+        $scope.keywordarray.splice(i,1);
+        i--;
+      }
+    }
+
+    console.log("keywordarray -> ", $scope.keywordarray);
+    console.log("shoppingList -> ", $scope.shoppingList);
+  }
+  $scope.keywordselected = function () {
+    console.log("not");
+  }
   $scope.checked = function () {
     var newShoppingList = [];
     for (var key in $scope.mythemes){
       if ($scope.mythemes.hasOwnProperty(key)){
         if ($scope.mythemes[key]){
           $scope.dummy = true;
-          for (var item in shoppingList){
-            if (shoppingList[item].theme === key){
-                newShoppingList.push(shoppingList[item]);
+          for (var item in $scope.keywordarray){
+            if ($scope.keywordarray[item].theme === key){
+                newShoppingList.push($scope.keywordarray[item]);
             }
           }
         }
       }
     }
     if ($scope.dummy === false){
-      $scope.shoppingList = shoppingList;
+      $scope.shoppingList = $scope.keywordarray;
     }
     else{
       $scope.shoppingList = newShoppingList;
